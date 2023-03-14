cb8a0a8ae258d06d413bf8651e7b6a839d503857
CauanDev <109750240+CauanDev@users.noreply.github.com>
2023-03-14 13:10:27 -0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,27 @@
+const List = 
+[
+    {
+        title: 'Vereador',
+        numeros:5,
+        vice: false,
+        candidatos: 
+        [
+            {ID:91453, Partido: "ABCD",Name: "João Marcos",Image: "images/84"},
+            {ID:35478, Partido: "EFGH",Name: "Marcos João",Image: "images/84_2"},
+        ]
+    },
+    {
+        title: 'Prefeito',
+        numeros:2,
+        vice: true,
+        candidatos:
+        [
+            {ID: 89, Partido: 'PSOL',Name:'Muhamed Ali',SecondName:'Michael Felix',FirstImage:'images/99',SecondImage:'images/99_2'},
+            {ID: 99, Partido: 'LOPS',Name:'Willian Bolt',SecondName:'Michele Bolt',FirstImage:'images/38111',SecondImage:'images/77222'},
+        ]
+
+
+    }
+
+
+]

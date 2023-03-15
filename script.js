70451b78a49b70789c429f2ed7ecefc2281671fe
CauanDev <109750240+CauanDev@users.noreply.github.com>
2023-03-15 14:59:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
 const title = document.querySelector('.divisao-1-esquerda-type')
 const images = document.querySelector('.divisao-1-direita')
+const imagebig = document.querySelector('.div-1-direita-img img');
+const imagesmall = document.querySelector('.div-1-direita-img-small')
 const information = document.querySelector('.divisao-1-esquerda-other')
 const divDigits = document.querySelector('.divisao-1-esquerda-numbers')
 const squares = document.querySelector('.divisao-1-esquerda-numbers .number')
-var index;
-var soma='';
+const votoBranco = document.querySelector('.voto-branco')
+var soma = ''
+
+
+
+
 
 function AtualizarInterface(n){
-   n = parseInt(n)
+    
+   n = parseInt(n)  
    soma = parseInt(soma)
+  
     switch(n){
+        
         case 1:{
-           let teste = List[0].candidatos.find((a) =>a.ID == soma)
-                  
+           let teste = List[0].candidatos.find((a,index) =>a.ID == soma)
+           if(teste != null ){
+            imagebig.src = `${teste.Image}`
+            images.style.display = 'flex'
+            document.querySelector('.div-1-direita-img span').innerHTML = List[0].title
+            information.style.display = 'flex'
+            information.innerHTML = `Nome: ${teste.Name}<br>Partido: ${teste.Partido}`
+            document.querySelector('.btn--confirma').addEventListener('click',()=>{
+                List[0].candidatos.find((a) =>{
+                    if(a.ID == soma ){
+                        votoBranco.style.display = 'none'
+                        a.Votos++;
+                        Prefeito();
+                    }
+                    else if(votoBranco.style.display == 'flex'){
+                        Prefeito();
+                    }
+                })
+            })
+           
+            
+            
+           }
+           else{
+                votoBranco.style.display = 'flex'
+           }
+           
             break;
+
         }
     }
 }
 
 function atribuir(n)
 {
+    
     const botoes = document.querySelectorAll('.teclado--linha .teclado--botao').forEach((e)=>
 {
     e.addEventListener('click',()=>
     {
         
         Digitar(e.textContent,n)
-
+        
         
         
     })
@@ -34,7 +70,8 @@ function atribuir(n)
 }
 function Digits(x)
 {    
-    for(let i=0;i<5;i++)
+    divDigits.innerHTML = ''
+    for(let i=0;i<x;i++)
     {      
         if(i==0)
         {
@@ -47,18 +84,20 @@ function Digits(x)
     }  
 }
 
-function Digitar(n){
+function Digitar(content,n){
     let numero = document.querySelector('.number.pisca')
-    
+   
     if(numero !== null)
     {
-        numero.innerHTML = n;       
-        soma += `${n}`        
+        numero.innerHTML = content;       
+        soma += `${content}`   
+        console.log(soma)     
         numero.classList.remove('pisca')
         if(numero.nextElementSibling!=null){
             numero.nextElementSibling.classList.add('pisca');
         }
          else{            
+            console.log(n)
             AtualizarInterface(n);
          }
     }
@@ -70,11 +109,29 @@ function Digitar(n){
 
 function Vereador()
 {
-    title.innerHTML = 'Vereador';
+    title.innerHTML = 'VEREADOR';
     images.style.display = 'none';
     information.style.display = 'none';
+    imagesmall.style.display = 'none'
+    votoBranco.style.display ='none'
     Digits(5);      
     atribuir(1)
+    document.querySelector('.btn--corrige').addEventListener('click',()=>{
+        soma = '';
+        votoBranco.style.display = 'none'
+        divDigits.innerHTML= ''
+        Digits(5); 
+    })
+
+    document.querySelector('.btn--branco').addEventListener('click',()=>{
+        votoBranco.style.display = 'flex'
+        divDigits.innerHTML= ''
+        document.querySelector('.btn--confirma').addEventListener('click',()=>{
+            votoBranco.style.display = 'none'
+            Prefeito();
+        })
+        
+    })
    
     
     
@@ -82,5 +139,9 @@ function Vereador()
     
 }
 
+function Prefeito(){
+    title.innerHTML = 'PREFEITO';
+    Digits(2); 
+}
 
 Vereador()

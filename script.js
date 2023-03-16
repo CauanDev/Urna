0500fe19bc578c408fbafc5d7f3f90fd1597ad1e
CauanDev <109750240+CauanDev@users.noreply.github.com>
2023-03-16 12:41:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,39 +6,59 @@ const information = document.querySelector('.divisao-1-esquerda-other')
 const divDigits = document.querySelector('.divisao-1-esquerda-numbers')
 const squares = document.querySelector('.divisao-1-esquerda-numbers .number')
 const votoBranco = document.querySelector('.voto-branco')
+const botoes = document.querySelectorAll('.teclado--linha .teclado--botao')
 var soma = ''
+var etapa;
+var indice;
 
 
 
 
+document.querySelector('.btn--branco').addEventListener('click',()=>{
+    information.innerHTML = ''
+    votoBranco.style.display = 'flex'
+    divDigits.innerHTML= ''
+    document.querySelector('.btn--confirma').addEventListener('click',()=>{
+        votoBranco.style.display = 'none'
+
+        if(etapa == 2){
+            alert('Obrigado pelo seu voto!');
+          Vereador();
+        }
+       if(etapa == 1){
+        alert('Obrigado pelo seu voto!');
+          Prefeito();
+       }
+        
+    })
+    
+})
+document.querySelector('.btn--corrige').addEventListener('click',()=>{
+    soma = '';
+    images.style.display = 'none'
+    votoBranco.style.display = 'none'
+    divDigits.innerHTML= ''
+    Digits(indice); 
+})
 
 function AtualizarInterface(n){
     
    n = parseInt(n)  
-   soma = parseInt(soma)
+   
+   
   
     switch(n){
         
-        case 1:{
-           let teste = List[0].candidatos.find((a,index) =>a.ID == soma)
-           if(teste != null ){
+        case 1:
+            somaUp = parseInt(soma)
+            teste = List[0].candidatos.find((a,index) =>a.ID == somaUp)
+            if(teste != null ){
             imagebig.src = `${teste.Image}`
             images.style.display = 'flex'
             document.querySelector('.div-1-direita-img span').innerHTML = List[0].title
             information.style.display = 'flex'
             information.innerHTML = `Nome: ${teste.Name}<br>Partido: ${teste.Partido}`
-            document.querySelector('.btn--confirma').addEventListener('click',()=>{
-                List[0].candidatos.find((a) =>{
-                    if(a.ID == soma ){
-                        votoBranco.style.display = 'none'
-                        a.Votos++;
-                        Prefeito();
-                    }
-                    else if(votoBranco.style.display == 'flex'){
-                        Prefeito();
-                    }
-                })
-            })
+          
            
             
             
@@ -47,21 +67,89 @@ function AtualizarInterface(n){
                 votoBranco.style.display = 'flex'
            }
            
-            break;
+           document.querySelector('.btn--confirma').addEventListener('click',()=>{
+            List[0].candidatos.find((a) =>{
+                if(a.ID == somaUp ){
+                    votoBranco.style.display = 'none'
+                    a.Votos++;
+                    Prefeito();
+                }
+                else{
+                    votoBranco.style.display = 'none'
+                    Prefeito();
+                }
+            })
+        })
+        
+        break;
+        
+        
+        case 2:
+            divDigits.style.display = 'flex'
+            votoBranco.style.display = 'none'
+            somaUp = parseInt(soma);
+            teste = List[1].candidatos.find((a,index) =>a.ID == somaUp)
+           
+            
+            if(teste != null ){
 
-        }
+            
+            imagebig.src = `${teste.FirstImage}`
+            document.querySelector('.div-1-direita-img span').innerHTML = List[1].title
+            document.querySelector('.div-1-direita-img-small img').src = `${teste.SecondImage}`
+            document.querySelector('.div-1-direita-img-small span').innerHTML = "Vice"
+            images.style.display = 'flex'
+            document.querySelector('.div-1-direita-img span').innerHTML = List[1].title
+            information.style.display = 'flex'
+            information.innerHTML = `Nome: ${teste.Name}<br>Partido: ${teste.Partido}`
+            document.querySelector('.btn--confirma').addEventListener('click',()=>{
+                List[1].candidatos.find((a) =>{
+                    if(a.ID == somaUp ){
+                        votoBranco.style.display = 'none'
+                        a.Votos++;
+                        alert('Obrigado pelo seu voto!');
+                        document.querySelector('.tela').innerHTML = ''
+                        
+                    }
+                    else{
+                        alert('Obrigado pelo seu voto!');
+                        document.querySelector('.tela').innerHTML = ''
+                        
+                    }
+                    
+                })
+            })
+           
+            
+            
+           }
+           else{
+                votoBranco.style.display = 'flex'
+                
+                document.querySelector('.btn--confirma').addEventListener('click',()=>{
+                    alert('Obrigado pelo seu voto!');
+                    document.querySelector('.tela').innerHTML = ''
+                    
+                })
+           }
+
+
+        
+        break;
     }
+   
+ 
 }
 
-function atribuir(n)
+function atribuir()
 {
     
-    const botoes = document.querySelectorAll('.teclado--linha .teclado--botao').forEach((e)=>
+     botoes.forEach((e)=>
 {
     e.addEventListener('click',()=>
     {
         
-        Digitar(e.textContent,n)
+        Digitar(e.textContent,etapa)
         
         
         
@@ -86,21 +174,24 @@ function Digits(x)
 
 function Digitar(content,n){
     let numero = document.querySelector('.number.pisca')
-   
+       
     if(numero !== null)
     {
+        
         numero.innerHTML = content;       
         soma += `${content}`   
-        console.log(soma)     
+         
         numero.classList.remove('pisca')
         if(numero.nextElementSibling!=null){
             numero.nextElementSibling.classList.add('pisca');
         }
-         else{            
-            console.log(n)
+         else{           
+            
             AtualizarInterface(n);
          }
     }
+
+    
     
     
 
@@ -112,26 +203,15 @@ function Vereador()
     title.innerHTML = 'VEREADOR';
     images.style.display = 'none';
     information.style.display = 'none';
-    imagesmall.style.display = 'none'
+    imagesmall.style.display ='none'
     votoBranco.style.display ='none'
-    Digits(5);      
-    atribuir(1)
-    document.querySelector('.btn--corrige').addEventListener('click',()=>{
-        soma = '';
-        votoBranco.style.display = 'none'
-        divDigits.innerHTML= ''
-        Digits(5); 
-    })
+    etapa = 1;
+    indice = 5;
+    Digits(indice);      
+    atribuir()
+    
 
-    document.querySelector('.btn--branco').addEventListener('click',()=>{
-        votoBranco.style.display = 'flex'
-        divDigits.innerHTML= ''
-        document.querySelector('.btn--confirma').addEventListener('click',()=>{
-            votoBranco.style.display = 'none'
-            Prefeito();
-        })
-        
-    })
+   
    
     
     
@@ -140,8 +220,25 @@ function Vereador()
 }
 
 function Prefeito(){
+    imagesmall.style.display ='flex'
+    images.style.display = 'none'
+    information.style.display = 'none'
+    etapa = 2;
     title.innerHTML = 'PREFEITO';
-    Digits(2); 
+    indice = 2;
+   
+    
+   
+    Digits(indice);    
+    soma = '';
+    
+    
+
+    
+   
+    
+    
+    
 }
 
 Vereador()
